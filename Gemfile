b4ea654ae9223e6af9636aecc77e84c324697e31
yuramon <yuramon2405@gmail.com>
2022-05-10 16:16:58 +0300

    base auth for sinatra

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'json'
+gem 'rake'
+
+group :test, :development do
+  gem 'pry-byebug'
+end
---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require './lib/middlewares/auth_middleware'
+
+class App < Sinatra::Base
+
+  use Rack::Auth::Basic do |username, password|
+    username == 'admin' && password == 'secret'
+  end
+
+  get '/' do
+    "Hello World"
+  end
+end
